03111378231ec3ec803e29c60bb7328514ea7620
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-30 19:08:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, character.motto FROM characters
-   ORDER BY characters.name, characters.motto;"
+   ORDER BY LENGTH(characters.motto) LIMIT 1;"
 end
 
 
