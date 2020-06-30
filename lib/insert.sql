8a410ce5fd24e04f50c2b6b36f519d6817ee26be
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-30 18:47:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -16,4 +16,7 @@ INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", 
 /* More Characters */
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
-/* */
+/* Character_Books */
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+
+/* More Character_Books */
