a64b4f2e6d6e8b15a90034a7fad592aff253b10b
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-30 18:56:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECTS books.title, books.year FROM books 
+  "SELECT books.title, books.year FROM books 
    LEFT OUTER JOIN series ON books.series_id = series.id
    WHERE series.id = 1;"
 end
